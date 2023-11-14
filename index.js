0f81fad92b4063c08c0655a9bcf1c286f06f7fb0
Nikita Torbinskiy <n.torbinskiy@gmail.com>
2023-11-14 13:13:31 +0200

    Update users logging

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http = require("http");
+
 const users = require("./users/index");
 
 http
-  .createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`);
-    res.write(users());
-    res.end();
-  })
-  .listen(process.env.PORT || 3000);
+	.createServer(function (req, res) {
+		console.log(`Just got a request at ${req.url}!`);
+		const usersList = users();
+		console.log(usersList);
+		res.write(JSON.stringify(usersList));
+
+		res.end();
+	})
+	.listen(process.env.PORT || 3000);
--- users/index.js
+++ users/index.js
@@ -1,23 +1,23 @@
 const casual = require("casual");
 
 module.exports = () => {
-  casual.define("user", function () {
-    return {
-      name: casual.first_name,
-      surname: casual.last_name,
-      address: casual.street,
-      phone: casual.phone,
-      email: casual.email,
-      postalCode: casual.zip,
-      city: casual.city,
-      number: casual.building_number,
-      id: casual.uuid,
-    };
-  });
-  const data = [];
-  // Create 100 users
-  for (let i = 0; i < 100; i++) {
-    data.push(casual.user);
-  }
-  return data;
+	casual.define("user", function () {
+		return {
+			name: casual.first_name,
+			surname: casual.last_name,
+			address: casual.street,
+			phone: casual.phone,
+			email: casual.email,
+			postalCode: casual.zip,
+			city: casual.city,
+			number: casual.building_number,
+			id: casual.uuid,
+		};
+	});
+	const data = [];
+	// Create 100 users
+	for (let i = 0; i < 2; i++) {
+		data.push(casual.user);
+	}
+	return data;
 };
